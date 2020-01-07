586f99c1c25d18edce097a617d6b2ccf93bc4aca
Amir Gul <amirgul@Amirs-MacBook-Pro.local>
2020-01-07 16:21:33 -0500

    30

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        for(int i = 1; i<= 5; i++)
+        {
+            if(i == 3)
+                System.exit(0);
+            System.out.println("Hello");
+        }
+        System.out.println("after the loop");
+    }
+}
